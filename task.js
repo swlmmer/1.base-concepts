2697e8b617bb3e159c8c11f11a1895655ae0ecb3
swlmmer <marsgalaktikov@yandex.ru>
2023-09-25 23:17:20 +0300

    Реализовано 1-е задание

---
+++ task.js
@@ -0,0 +1,19 @@
+"use strict"
+function solveEquation(a, b, c) {
+  let arr = [x, x1, x2];
+  let a = 1;
+  let b = 2;
+  let c = 3;
+  let d = Math.pow(b, 2) - 4*a*c;
+  let v = arr;
+  let data = arr[i];
+   let root = data[0];
+   let root1 = data[1];
+   let root2 = data[2];
+  
+  return arr;
+}
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  
+}
